c4c2666cd8e7ef657249c69f32c72d11a99356b8
thomphav <thomascarloshogan@gmail.com>
2024-03-01 19:12:19 -0800

    responsive lobby

--- app/javascript/home/index.tsx
+++ app/javascript/home/index.tsx
@@ -39,21 +39,21 @@ const Home = ({ initialGames }: { initialGames: game[] }) => {
 
   return (
     <>
-      <div className="flex w-full h-full items-center p-24 space-x-24">
-        <div className="flex h-full items-center justify-center w-[380px] max-w-[380px] min-w-[380px]">
-          <form className="flex flex-col justify-center items-center mx-auto space-y-16" action="/games" method="post">
-            <h1 className="text-4xl">
+      <div className="flex flex-col lg:flex-row w-full h-full items-start lg:items-center px-8 py-8 sm:px-16 sm:py-16 space-x-0 space-y-16 sm:space-y-8 lg:space-x-24 lg:space-y-0">
+        <div className="flex h-fit lg:h-full items-center justify-center w-full lg:w-[380px] lg:max-w-[380px] lg:min-w-[380px]">
+          <form className="flex flex-col sm:flex-row lg:flex-col w-full justify-between lg:justify-center items-center mx-auto space-y-8 space-x-0 sm:space-y-0 sm:space-x-16 lg:space-y-16 lg:space-x-0" action="/games" method="post">
+            <h1 className="text-3xl lg:text-4xl">
               <span className="text-purple-600">9</span> Players Online
             </h1>
             <CsrfInput />
-            <button className="border border-gray-300 py-4 px-6 w-full text-xl rounded-md hover:bg-gray-50" type="submit">Create New Game</button>
+            <button className="border border-gray-300 py-4 px-6 w-full max-w-[200px] lg:max-w-full text-base lg:text-xl rounded-md hover:bg-gray-50" type="submit">Create New Game</button>
           </form>
         </div>
         <div className="flex justify-center w-full h-full">
           <div className="flex flex-col space-y-2 h-full w-full items-center border border-gray-200 rounded p-2 overflow-y-scroll">
             {games.length === 0 ?
               <div className="flex h-full items-center">
-                <p className="text-lg">No games are live at the moment</p>
+                <p className="text-base sm:text-lg">No games are live at the moment</p>
               </div>
             :
               games.map((game: game) => (
@@ -61,8 +61,8 @@ const Home = ({ initialGames }: { initialGames: game[] }) => {
                   key={game.id} href={`/games/${game.id}/room`}
                   className="border border-gray-200 rounded w-full p-4 flex items-center hover:bg-gray-100 justify-between"
                 >
-                  <span>Players: {game.room_count}</span>
-                  <span>Created {timeSince(game.created_at)}</span>
+                  <span className="text-sm sm:text-base">Players: {game.room_count}</span>
+                  <span className="text-sm sm:text-base">Created {timeSince(game.created_at)}</span>
                 </a>
               ))
             }
